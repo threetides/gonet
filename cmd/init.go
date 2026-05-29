f333c4dd3638b735d90c4bfec7dfbe1f5f3c04ba
JonasLillebo <lillebohaugen@gmail.com>
2026-05-30 01:19:07 +0200

    Implement init command to create a new project directory

--- cmd/init.go
+++ cmd/init.go
@@ -5,6 +5,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +22,18 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		var projectName string
+
+		log.Print("Project name: ")
+		fmt.Scanln(&projectName)
+
+		// Create root dir
+		err := os.Mkdir(projectName, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Created project %s\n", projectName)
 	},
 }
 
