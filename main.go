21d1640249aefbb33fc6a42cd2a1c25998706b82
JonasLillebo <lillebohaugen@gmail.com>
2026-05-30 00:19:47 +0200

    Update GoReleaser version to latest and clean up main.go

--- main.go
+++ main.go
@@ -4,11 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Gopher!")
-	fmt.Println("First patch")
-	fmt.Println("Second patch")
-	fmt.Println("Third patch")
-	fmt.Println("Fourth patch")
-	fmt.Println("Fift patch")
-	fmt.Println("Sixt patch")
-	fmt.Println("Seventh patch")
 }
